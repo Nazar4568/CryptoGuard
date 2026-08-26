b348ed5d7236259da42ae0eb3c76da2d288f6cfe
Nazarii FIlin <nazariifilin@gmail.com>
2026-08-26 16:55:31 +0200

    added way to skip rule check for lines with # cryptoguard: ignore

--- test_code.py
+++ test_code.py
@@ -1,8 +1,8 @@
 import hashlib
-import random
+import random # cryptoguard: ignore
 
 def verify_file(file_content):
-    return hashlib.md5(file_content).hexdigest()
+    return hashlib.md5(file_content).hexdigest() # cryptoguard: ignore
 
 def hash_user_password(password_string):
     return hashlib.md5(password_string.encode()).hexdigest() # cryptoguard: ignore
